87cd009a2d22d3942b038ff8fb916c0d4ca15694
Tennison <tennison35@tennsion-mac-mini.local>
2014-01-27 15:38:33 +0800

    [structure change] changing for a better structure

--- blackout.js
+++ blackout.js
@@ -1,116 +1,124 @@
 javascript: (function() {
-	cssList = {
-		"#coveringCanvas": {
-			"position": "absolute",
-			"top": 0,
-			"left": 0,
-			"background-color": "rgba(255,255,255,.1)",
-			"z-index": 9999999
-		},
-	};
 
-	function canvasOverlay() {
-		var canvasWidth = window.innerWidth,
-			canvasHeight = window.innerHeight,
-			isDrawing;
+    var startBookmarklet = function($) {
 
-		function midPointBtw(p1, p2) {
-			return {
-				x: p1.x + (p2.x - p1.x) / 2,
-				y: p1.y + (p2.y - p1.y) / 2
-			};
-		}
+        function init(){
+            prepareSimpleCanvas();
+            var locker = new Locker();
+            var lockPos = locker.lock().pos;
+            cssSetUP(cssList);
+            cssSetUP({ "#coveringCanvas": lockPos });
+            window.blackoutIsBuilt = true;
+        }
 
-		function prepareSimpleCanvas() {
-			$('body').append($('<div id="divCanvas"></div>'));
-			var canvasDiv = document.getElementById('divCanvas');
-			canvas = document.createElement('canvas');
-			canvas.setAttribute('width', canvasWidth);
-			canvas.setAttribute('height', canvasHeight);
-			canvas.setAttribute('id', 'coveringCanvas');
-			canvasDiv.appendChild(canvas);
-			if (typeof G_vmlCanvasManager != 'undefined') {
-				canvas = G_vmlCanvasManager.initElement(canvas);
-			}
-			context = canvas.getContext("2d");
-			context.lineWidth = 15;
-			context.lineJoin = 'miter';
-			context.lineCap = 'butt';
-			var isDrawing, points = [], line = [];
+        var cssList = {
+            "#coveringCanvas": {
+                "position": "absolute",
+                "top": 0,
+                "left": 0,
+                "background-color": "rgba(255,255,255,.1)",
+                "z-index": 9999999
+            },
+        },
 
-			canvas.onmousedown = function(e) {
-				isDrawing = true;
-				points.push( [ { x: e.clientX, y: e.clientY } ] );
-			};
+        prepareSimpleCanvas = function () {
+            var isDrawing, points = [], line = [],
+                canvasDiv = $('<div id="divCanvas"></div>'),
+                canvas = document.createElement('canvas');
+            canvas.setAttribute('width', window.outerWidth);
+            canvas.setAttribute('height', window.outerHeight);
+            canvas.setAttribute('id', 'coveringCanvas');
+            $('body').append(canvasDiv);
+            canvasDiv.append(canvas);
+            if (typeof G_vmlCanvasManager != 'undefined') {
+                canvas = G_vmlCanvasManager.initElement(canvas);
+            }
+            context = canvas.getContext("2d");
+            context.lineWidth = 15;
+            context.lineJoin = 'miter';
+            context.lineCap = 'butt';
 
-			canvas.onmousemove = function(e) {
-				if (!isDrawing) return;
+            var midPointBtw = function (p1, p2) {
+                return {
+                    x: p1.x + (p2.x - p1.x) / 2,
+                    y: p1.y + (p2.y - p1.y) / 2
+                };
+            };
 
-				context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+            canvas.onmousedown = function(e) {
+                isDrawing = true;
+                points.push( [ { x: e.clientX, y: e.clientY } ] );
+            };
 
-				points[points.length-1].push({ x: e.clientX, y: e.clientY });
+            canvas.onmousemove = function(e) {
+                if (!isDrawing) return;
 
-				for(var j = 0, l = points.length; j < l; j++){
-					var p1, p2, midPoint;
-					line = points[j];
+                context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
-					context.beginPath();
-					context.moveTo(line[0].x, line[0].y);
+                points[points.length-1].push({ x: e.clientX, y: e.clientY });
 
-					for (var i = 0, len = line.length; i < len - 1; i++) {
-						p1 = line[i];
-						p2 = line[i+1];
-						midPoint = midPointBtw(p1, p2);
-						context.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
-					}
-					context.lineTo(p1.x, p1.y);
-					context.stroke();
-				}
-			};
+                for(var j = 0, l = points.length; j < l; j++){
+                    var p1, p2, midPoint;
+                    line = points[j];
 
-			canvas.onmouseup = function(e) {
-				isDrawing = false;
-			};
-		}
+                    context.beginPath();
+                    context.moveTo(line[0].x, line[0].y);
 
-		prepareSimpleCanvas();
-	}
+                    for (var i = 0, len = line.length; i < len - 1; i++) {
+                        p1 = line[i];
+                        p2 = line[i+1];
+                        midPoint = midPointBtw(p1, p2);
+                        context.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
+                    }
+                    context.lineTo(p1.x, p1.y);
+                    context.stroke();
+                }
+            };
 
-	function Locker () {
-		var lockX, lockY;
+            canvas.onmouseup = function(e) {
+                isDrawing = false;
+            };
+        },
 
-		function lockIt() {
-			window.scrollTo(lockX,lockY);
-			return false;
-		}
+        Locker = function() {
+            var lockX, lockY;
 
-		return {
-			lock: function() {
-				lockX = window.scrollX;
-				lockY = window.scrollY;
+            function lockIt() {
+                window.scrollTo(lockX,lockY);
+                return false;
+            }
 
-				window.addEventListener("scroll", lockIt, false);
-				return {
-					pos: {
-						left: lockX,
-						top: lockY
-					}
-				}
-			},
-			unlock : function() {
-				window.removeEventListener("scroll", lockIt, false);
-			}
-		}
-	}
+            return {
+                lock: function() {
+                    lockX = window.scrollX;
+                    lockY = window.scrollY;
 
-	function cssSetUP(list) {
-		for (var item in list) {
-			var selector = $(item);
-			if (selector) {
-				selector.css(list[item]);
-			}
-		}
-	}
+                    window.addEventListener("scroll", lockIt, false);
+                    return {
+                        pos: {
+                            left: lockX,
+                            top: lockY
+                        }
+                    };
+                },
+                unlock : function() {
+                    window.removeEventListener("scroll", lockIt, false);
+                }
+            };
+        },
+
+        cssSetUP = function (list) {
+            var item, selector;
+            for (item in list) {
+                    selector = $(item);
+                if (selector) {
+                    selector.css(list[item]);
+                }
+            }
+        };
+
+        if(!window.blackoutIsBuilt) init();
+    };
 
 	/**
 		* Google Analytics JS v1
@@ -149,24 +157,16 @@ javascript: (function() {
 		})("UA-13280906-3", "tennison35.github.io", "blackout.js");
 	}
 
-	function main($) {
-		canvasOverlay();
-		var locker = new Locker();
-		var lockPos = locker.lock().pos;
-		cssSetUP(cssList);
-		cssSetUP({ "#coveringCanvas": lockPos });
-	}
-
 	if (!window.jQuery) {
 		var head = document.getElementsByTagName("head")[0],
 			jQueryScript = document.createElement("script");
 		jQueryScript.type = "text/javascript";
 		jQueryScript.src = "http://code.jquery.com/jquery-1.10.2.min.js";
 		jQueryScript.onload = function() {
-			main(window.jQuery);
+			startBookmarklet(window.jQuery);
 		};
 		head.appendChild(jQueryScript);
 	} else {
-		main(window.jQuery);
+		startBookmarklet(window.jQuery);
 	}
 })();
