75d0459800159a43216afe4228d1a0729d420a74
Tennison Chan <tennison35@gmail.com>
2014-02-01 15:25:10 +0800

    [gnmenu][bookmarklet] add gnmenu script src in bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -7,11 +7,14 @@ bookmarklet.anchor = null;
 bookmarklet.jQuery = null;
 
 bookmarklet.scripts = [
-  { "el":"script", "attr": {"type": "text/javascript", "id": "bml-jquery", "src": "http://code.jquery.com/jquery-1.10.2.min.js"} },
-  /*{ "el":"script", "attr": {"type": "text/javascript", "id": "bml-blackout", "src": "http://localhost/blackout/blackout.js"} },*/
-  { "el":"script", "attr": {"type": "text/javascript", "id": "bml-blackout", "src": "http://localhost/blackout/graffita.js"} },
-  { "el":"script", "attr": {"type": "text/javascript", "id": "bml-paperjs", "src": "http://cdnjs.cloudflare.com/ajax/libs/paper.js/0.9.12/paper.js"} },
-  { "el":"link", "attr": {"type": "text/css", "rel": "stylesheet", "id": "bml-css", "href": "http://localhost/blackout/blackout.css"} }
+  { "el":"script", "attr": {"type": "text/javascript", "id": "bml-jquery", "src": "http://127.0.0.1/blackout/lib/jquery-1.10.2.min.js"} },
+  /*{ "el":"script", "attr": {"type": "text/javascript", "id": "bml-blackout", "src": "http://127.0.0.1/blackout/blackout.js"} },*/
+  { "el":"script", "attr": {"type": "text/javascript", "id": "bml-blackout", "src": "http://127.0.0.1/blackout/graffita.js"} },
+  { "el":"script", "attr": {"type": "text/javascript", "id": "bml-paperjs", "src": "http://127.0.0.1/blackout/lib/paper.js"} },
+  { "el":"script", "attr": {"type": "text/javascript", "id": "bml-paperjs", "src": "http://127.0.0.1/blackout/gnmenu.js"} },
+  { "el":"link", "attr": {"type": "text/css", "rel": "stylesheet", "id": "bml-css", "href": "http://127.0.0.1/blackout/css/blackout.css"} },
+  { "el":"link", "attr": {"type": "text/css", "rel": "stylesheet", "id": "bml-css", "href": "http://127.0.0.1/blackout/css/gnmenu.css"} },
+  { "el":"script", "attr": {"type": "text/javascript", "id": "bml-menu", "src": "http://127.0.0.1/blackout/gnmenu-template.js"} }
 ];
 
 bookmarklet.loadScript = function(opt, onload) {
@@ -62,10 +65,14 @@ bookmarklet.toggle = function(){
 
 bookmarklet.app = function($){
   /*for(var i=1, l = bookmarklet.scripts.length;i < l;i++){*/
-    bookmarklet.loadScript(bookmarklet.scripts[2], function(){
-      bookmarklet.loadScript(bookmarklet.scripts[1]);
+    bookmarklet.loadScript(bookmarklet.scripts[3], function(){
+      bookmarklet.loadScript(bookmarklet.scripts[2], function(){
+        bookmarklet.loadScript(bookmarklet.scripts[1]);
+      });
     });
-    bookmarklet.loadScript(bookmarklet.scripts[3]);
+    bookmarklet.loadScript(bookmarklet.scripts[4]);
+    bookmarklet.loadScript(bookmarklet.scripts[5]);
+    bookmarklet.loadScript(bookmarklet.scripts[6]);
   /*}*/
 };
 
